f3f428ea4f17746b3298cc9b86e016e409f40863
Gabriele Rinaldis <32817747+rigabri@users.noreply.github.com>
2021-11-11 10:24:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+   int n = 0;
+   cin >> n;
+   int a = 1;
+   int b = 1;
+   while (a < n) {
+      cout << a << endl;
+      int s = a + b;
+      a = b;
+      b = s;
+   }
    return 0;
 }
